d6320905d50e40047d9cc4e24cd38fb94e53cc01
Michael Dawson <michael_dawson@ca.ibm.com>
2020-05-10 13:17:44 -0400

    feat: expand coverage of support specification
    - expand coverage of options for how to specify support
      file location
    - add initial cli based test suite
    
    Signed-off-by: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/index.js
+++ test/index.js
@@ -5,6 +5,8 @@ const util = require('util')
 const fs = require('fs-extra')
 const Ajv = require('ajv')
 const support = require('..')
+const childProcess = require('child_process')
+const assert = require('assert')
 
 // Delay so we can load in the example files
 ;(async () => {
@@ -36,6 +38,22 @@ const support = require('..')
           console.error(util.inspect(e, false, null, true))
           throw e
         }
+      })
+    })
+  })
+
+  const cliTestsDir = path.join(__dirname, 'cli')
+  const cliTests = await fs.readdir(cliTestsDir)
+  suite('cli tests', async () => {
+    const supportCommand = '../../../bin/support'
+    cliTests.forEach((testDir) => {
+      test(`cli test: ${testDir}`, () => {
+        const cwd = path.join(__dirname, 'cli', testDir)
+        const rawCommand = fs.readFileSync(path.join(cwd, 'command')).toString()
+        const command = rawCommand.replace(/\$\{cwd\}/g, cwd)
+        const expected = fs.readFileSync(path.join(cwd, 'expected')).toString()
+        const result = childProcess.execSync(`${supportCommand} ${command}`, { cwd: cwd }).toString()
+        assert.strictEqual(result, expected)
       })
     })
   })
